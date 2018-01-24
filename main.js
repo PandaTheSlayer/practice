f7253f3d9d54823360dfd65252ea49be7745ee5a
unknown <nnkvizitor@gmail.com>
2018-01-24 07:15:07 +0200

    Add functionality

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
 document.addEventListener("DOMContentLoaded", function(){
+    const questions = $('.question-row');                 // Получение объекта каждого вопроса
+
     $('#draw-btn').on('click', function(){
-        console.log(calculateAnsSum());
+        ans_sum = calculateAnsSum(questions);             // Получаем массив сумм ответов на каждый из вопросов
+        //console.log(ans_sum);
+        console.log(getVarAns(questions));
     })
 });
 
-function calculateAnsSum() {
-    var questions = $('.question-row');                 // Получение объекта каждого вопроса
-    var ans_qt = [];                    
+function calculateAnsSum(questions) {
+
+    let ans_sum = [];                    
     questions.each(function(i, elem){
-        var ans = 0;
-        $(this).find('.answers')                        // Поиск значения количества ответов
+        let ans = 0;
+        $(this).find('.answers')                          // Поиск значения количества ответов
                 .each(function(i, elem){
                     ans = ans + parseInt($(this).val());
         });
-        ans_qt.push(ans);
+        ans_sum.push(ans);
     })
-    return ans_qt;
+    return ans_sum;
 }
 
+function getVarAns(questions){
+    let questions_ans = [];
+    questions.each(function(){             // Проход по каждому вопросу
+        let ans = [];
+        $(this).find('.answers').          // Выборка каждого варианта
+                each(function(){
+                    ans.push($(this).val());
+                })
+            questions_ans.push(ans);
+    })
+    return questions_ans;
+}
