cc4caf7826e798664b010752c16168615780c84a
unknown <nnkvizitor@gmail.com>
2018-01-24 08:30:17 +0200

    Added functionality

--- main.js
+++ main.js
@@ -2,11 +2,30 @@ document.addEventListener("DOMContentLoaded", function(){
     const questions = $('.question-row');                 // Получение объекта каждого вопроса
 
     $('#draw-btn').on('click', function(){
-        ans_sum = calculateAnsSum(questions);             // Получаем массив сумм ответов на каждый из вопросов
-        //console.log(ans_sum);
-        console.log(getVarAns(questions));
+        ans_sum = calculateAnsSum(questions);             // Количество ответов на каждый вопрос
+        var_ans = getVarAns(questions);                   // Количество ответов на каждый из вариантов вопроса
+        var_weights = getVarWeights(questions);           // Вес каждого варианта ответа
+
+        console.log(ans_sum);
+        console.log(var_ans);
+        console.log(var_weights);
+
+        var k = calculateKs(var_ans, ans_sum);
+
+        console.log(k);
     })
 });
+
+function calculateKs(var_ans, ans_sum) {
+    var k = [];
+    var_ans.forEach((val, key) => {
+        k[key] = [];
+        val.forEach((val2) => {
+            k[key].push(val2 / ans_sum[key]);
+        });
+    });
+    return k;
+}
 
 function calculateAnsSum(questions) {
 
@@ -33,4 +52,17 @@ function getVarAns(questions){
             questions_ans.push(ans);
     })
     return questions_ans;
+}
+
+function getVarWeights(questions){
+    let questions_weights = [];
+    questions.each(function(){             // Проход по каждому вопросу
+        let weights = [];
+        $(this).find('.weights').          // Выборка каждого варианта
+                each(function(){
+                    weights.push($(this).val());
+                })
+            questions_weights.push(weights);
+    })
+    return questions_weights;
 }
