8476e52ecf175f3be06006a19e8b7a5b2f2d0ef6
unknown <nnkvizitor@gmail.com>
2018-02-16 03:57:53 +0200

    Small refactoring

--- objective-params/index.php
+++ objective-params/index.php
@@ -4,17 +4,13 @@
     $db->set_charset('utf8');
     $res = $db->query("SELECT * FROM `mdl_book` WHERE `course` = '14'");
 
-    while($row = $res->fetch_assoc()){
-        $books[] = $row;
-    }
+    $books = resultToAssocArray($res);
 
     //echo 'Количество лекций курса: ' . count($books);
 
     $grade_items = $db->query("SELECT * FROM `mdl_grade_items` WHERE `courseid`='14'");
 
-    while($row = $grade_items->fetch_assoc()){
-        $items[] = $row;
-    }
+    $items = resultToAssocArray($grade_items);
 
     //echo json_encode($items, JSON_UNESCAPED_UNICODE);
 
@@ -27,3 +23,9 @@
     };
 
     echo json_encode( array_map($mapper, $items), JSON_UNESCAPED_UNICODE );
+
+    function resultToAssocArray($result){
+        while($row = $result->fetch_assoc())
+            $arr[] = $row;
+        return $arr;
+    }
